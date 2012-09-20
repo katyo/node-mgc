2fdaaffe30c6172c50eb96c30483ba29512aca6f
Kayo Phoenix <kayo@illumium.org>
2012-09-20 19:39:26 +0600

    Simple print type util added..

---
+++ test/print.js
@@ -0,0 +1,13 @@
+var MGC = require('../mgc'),
+FS = require('fs');
+
+var mgc = new MGC('t');
+
+mgc.load(function(err){
+  if (err) throw err;
+  mgc.attach(FS.createReadStream(process.argv[2]))
+    .on('magic', function(err, res){
+      if(err) throw err;
+      console.log(res);
+    });
+});

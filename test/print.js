6efbd026cbd0f81f68cb798eed9972547e8feed0
Kayo Phoenix <kayo@illumium.org>
2012-09-21 11:04:36 +0600

    Test/Print fixed..

--- test/print.js
+++ test/print.js
@@ -1,13 +1,14 @@
 var MGC = require('../mgc'),
 FS = require('fs');
 
-var mgc = new MGC('t');
+var mgc = new MGC('type');
 
 mgc.load(function(err){
   if (err) throw err;
   mgc.wrap(FS.createReadStream(process.argv[2]))
     .on('magic', function(err, res){
       if(err) throw err;
+      if(res.search('/') < 0) throw new Error(process.argv[2] + ': "' + res + '" not mime');
       console.log(res);
     });
 });
